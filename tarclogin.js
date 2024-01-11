c80f8ef500954ca6d5e83c4aea8ed6a66db60629
ShibhamKumarGupta <shivamlovesu2forever@gmail.com>
2024-01-11 17:45:44 +0530

    first Javascript file added

---
+++ tarclogin.js
@@ -0,0 +1,66 @@
+var navlogin = `<nav class="navbar navbar-expand-lg navigation1">
+<div class="container">
+    <a class="navbar-brand" href="Foodies-home.html"><img src="images/logo.png" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-stream navbar-toggler-icon"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" area-current="page" href="Foodies-home.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Foodies-menu.html">Menu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Foodies-about.html">About Us</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="Foodies-faq.html">FAQ</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="Foodies-contact.html">Contact Us</a>
+            </li>
+            <li><button class="main-btn" onclick="logout();">
+                    Logout</button>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>`;
+
+var navlogout = `<nav class="navbar navbar-expand-lg navigation1">
+<div class="container">
+<a class="navbar-brand" href="Foodies-home.html"><img src="images/logo.png" alt="logo"></a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+<i class="fas fa-stream navbar-toggler-icon"></i>
+</button>
+<div class="collapse navbar-collapse" id="navbarText">
+<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+<li class="nav-item">
+    <a class="nav-link active" area-current="page" href="Foodies-home.html">Home</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="Foodies-menu.html">Menu</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="Foodies-about.html">About Us</a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href="Foodies-faq.html">FAQ</a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href="Foodies-contact.html">Contact Us</a>
+</li>
+<li><button class="main-btn" onclick="loginhome();">
+        Login/Register</button></li>
+</ul>
+</div>
+</div>
+</nav>`;
